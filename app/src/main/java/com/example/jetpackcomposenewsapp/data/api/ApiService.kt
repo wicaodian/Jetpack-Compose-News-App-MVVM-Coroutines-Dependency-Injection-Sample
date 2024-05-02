38690791238e03a6f60a9fe34c0f35cedfb675ff
Asad Mehmood <wicaodian@gmail.com>
2024-05-02 11:20:37 +0500

    refactored

--- app/src/main/java/com/example/jetpackcomposenewsapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/jetpackcomposenewsapp/data/api/ApiService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 interface ApiService {
 
     // Goto https://newsapi.org/ to get your key and add it to here
-    
+
     @GET("v2/top-headlines")
    suspend fun getNewsHeadline(
             @Query("country") country: String, @Query("apiKey") apiKey: String = ""
