a1546bb09920b36c740883dd7c34b8395fe090e4
Asad Mehmood <wicaodian@gmail.com>
2024-05-02 11:18:51 +0500

    comments updated

--- app/src/main/java/com/example/jetpackcomposenewsapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/jetpackcomposenewsapp/data/api/ApiService.kt
@@ -7,10 +7,12 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
+
+    // Goto https://newsapi.org/ to get your key and add it to here
+    
     @GET("v2/top-headlines")
    suspend fun getNewsHeadline(
-            @Query("country") country: String, @Query("apiKey") apiKey: String = "adb7e971b582499a9a720f4b204b71cd"
+            @Query("country") country: String, @Query("apiKey") apiKey: String = ""
     ): Response<NewsResponse>
 }
 
-//GET https://newsapi.org/v2/top-headlines?country=us&apiKey=adb7e971b582499a9a720f4b204b71cd
